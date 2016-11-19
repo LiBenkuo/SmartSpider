b525beacddd31eb9a47cb67020dde72f37872604
LiBenkuo <libenkuo@gmail.com>
2016-11-19 23:04:33 +0800

    Add dir Learn Python The Hard Way

--- BeautifulSoup/gyzyfc.py
+++ BeautifulSoup/gyzyfc.py
@@ -9,52 +9,57 @@ from xlwt import Workbook
 
 try:
 
-	url = "http://www.gyfc.net.cn/pro_query/index/floorView.aspx?dongID=38395&danyuan=&qu=%B9%F3%D1%F4&yszh=2015120"
-	url = "http://www.gyfc.net.cn/pro_query/index/floorView.aspx?dongID=13003394&danyuan=%25C8%25AB%25B2%25BF&qu=%25B0%25D7%25D4%25C6&yszh=2013055"
+	url = "http://www.gzzyfc.com.cn/Client/ZunYi/Second/Detail/ProjectInfo/FloorHouse.aspx?id=nw6wrIdxdG%2bLhp2Ca%2b6tUnlnRqkh0eqtCLXj%2f9KYzJ6NvQnefxIeyZ0EjKlupA8RaBKdDAy4CSvH64JmXbcz4A%3d%3d"
 
 	# 伪造浏览器请求头
 	headers = {
 		'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7'
 	}
 
+	cookie = "ASP.NET_SessionId=tsqpsaaae5sidexmckttbvwo"
+
 	request = urllib2.Request(
 		url = url,
 		headers = headers
 	)
+
+	request.add_header("Cookie", cookie)
 	response = urllib2.urlopen(request)
 
-	html = unicode(response.read(), 'gb2312').encode('utf-8')
+	html = response.read()
 
 	soup = BeautifulSoup(html, 'html.parser')
 
 	# 格式化输出
-	#print soup.prettify()
+	# print soup.prettify()
 
 	book = Workbook(encoding='utf-8')
 	sheet = book.add_sheet('销售信息')
 
 	rowIndex = 0
-	for saleInfo in soup.find_all('div', class_=re.compile('P0 W[1-9]|10 H0')):
-		# print saleInfo
+	for saleInfo in soup.find_all('div', id=re.compile('\d{16}')):
 
-		if(saleInfo.find('span') == None):
-			continue
-
-		roomCode = saleInfo.find('span').string
+		# 子节点
+		child = saleInfo.find('div')
 
 		columnIndex = 0
-		sheet.write(rowIndex, columnIndex, roomCode)
-		#columnIndex += 1
-		#sheet.write(rowIndex, columnIndex, roomCode[2:])
-		
-		for i in saleInfo['title'].split():
+
+		for spanInfo in child.find('div').find_all('span'):
+			#print spanInfo.string
+			sheet.write(rowIndex, columnIndex, spanInfo.string)
 			columnIndex += 1
-			sheet.write(rowIndex, columnIndex, i)
+
+		sheet.write(rowIndex, columnIndex, child.contents[1])
+		columnIndex += 1
+
+		# print child.contents[3]
+		sheet.write(rowIndex, columnIndex, child.contents[3])
+		
 			
 		rowIndex += 1	
 
 	# 文件保存
-	book.save('泰祥国际无单元.xls')
+	book.save('1.xls')
 	book.save(TemporaryFile())
 			
 except urllib2.HTTPError, e:
